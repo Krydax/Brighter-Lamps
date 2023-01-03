56bfc3a9d31532c5030281917b1c14b574249c40
Krydax <93162034+Krydax@users.noreply.github.com>
2023-01-03 11:31:47 -0500

    comma

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend({
         type = "int-setting",
         name = "light-size",
         setting_type = "startup",
-        default_value = 70
+        default_value = 70,
         minimum_value = 10,
         maximum_value = 10000
     }
